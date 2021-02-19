02c73c3662e121b6d6b8633d4ec9b4432aceea26
Gabriel Cruz <gabriel.cruzjoa@gmail.com>
2021-02-19 13:29:33 -0400

    Login App

---
+++ Login App/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginScreen();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
