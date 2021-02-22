be1a3d4c1df383a295dea408e667e7750e0ec421
Gabriel Cruz <gabriel.cruzjoa@gmail.com>
2021-02-21 23:51:36 -0400

    Login App MVVM

---
+++ Login App/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Login_App.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Login_App.ViewModels
+{
+    class SignUpViewModel
+    {
+        public ICommand OnSignUpCommand { get; }
+        public string NameSignUp { get; set; }
+        public string EmailSignUp { get; set; }
+        public string PasswordSignUp { get; set; }
+        public string ConfirmPassSignUp { get; set; }
+        public SignUpViewModel()
+        {
+            OnSignUpCommand = new Command(OnSignUpTapped);
+        }
+
+        private async void OnSignUpTapped()
+        {
+            if (CheckInfoSignUp())
+            {
+                await App.Current.MainPage.DisplayAlert("Alerta", "Debe completar todos los campos", "Cerrar");
+            }
+            if (PasswordSignUp != ConfirmPassSignUp)
+            {
+                await App.Current.MainPage.DisplayAlert("Alerta", "Las contraseñas deben ser iguales", "Cerrar");
+            }
+            if (!CheckInfoSignUp() && PasswordSignUp == ConfirmPassSignUp)
+            {
+                await App.Current.MainPage.DisplayAlert("Bienvenido", "Hola " + NameSignUp, "Cerrar");
+                await App.Current.MainPage.Navigation.PushModalAsync(new HomePage());
+            }
+        }
+        public bool CheckInfoSignUp()
+        {
+            if (string.IsNullOrEmpty(NameSignUp) || string.IsNullOrEmpty(EmailSignUp) || string.IsNullOrEmpty(PasswordSignUp))
+                return true;
+            else return false;
+        }
+    }
+}
---
+++ Login App/Views/HomeDetailPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login_App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeDetailPage : ContentPage
+    {
+        public HomeDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
