5d9108dc5f24ab97ef10619dcd0df4ba7d937744
Gabriel Cruz <gabriel.cruzjoa@gmail.com>
2021-02-21 23:44:16 -0400

    LoginAppMVVM

---
+++ Login App/Constant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Login_App
+{
+    public class Constant
+    {
+        public static bool IsDev = true;
+
+        public static Color BackgroundColor = Color.White;
+        public static Color MainTextColor = Color.White;
+        public static int LoginLogoHeight = 120;
+    }
+}
---
+++ Login App/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Login_App.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }     
+    }
+}
